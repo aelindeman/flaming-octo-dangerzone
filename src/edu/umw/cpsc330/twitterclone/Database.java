d166e44f62ef46c3b96ee46a3f3f536025caa90b
Alex Lindeman <alex@ael.me>
2012-11-01 16:55:05 -0400

    basic query() and close() for Database

--- src/edu/umw/cpsc330/twitterclone/Database.java
+++ src/edu/umw/cpsc330/twitterclone/Database.java
@@ -1,7 +1,6 @@
 package edu.umw.cpsc330.twitterclone;
 
 import java.sql.*;
-import java.util.Date;
 
 /**
  * Class for the database of users and posts.
@@ -36,6 +35,33 @@ public class Database
 			System.err.println("Caught exception while attempting to use database driver: " + e.getMessage());
 		} catch (SQLException e) {
 			System.err.println("Caught exception while connecting to database: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Performs a query on the database
+	 * @param query SQL query
+	 * @return results from query
+	 */
+	public ResultSet query(String query) throws SQLException
+	{
+		Statement st = db.createStatement();
+		st.setQueryTimeout(30);
+		
+		ResultSet result = st.executeQuery(query);
+		
+		return result;
+	}
+	
+	/**
+	 * Closes the connection to the database
+	 */
+	public void close() {
+		try {
+			if (db != null)
+				db.close();
+		} catch (SQLException e) {
+			System.err.println ("Caught exception while closing connection to database: " + e.getMessage());
 		}
 	}
 }
